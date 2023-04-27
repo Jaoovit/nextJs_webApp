53ecf694f2db20f5f0f6b546bf1272040a05e7b6
Joao Oliveira <jaoovit@gmail.com>
2023-04-27 12:56:03 +0100

    create a new page

---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+export default function fornecedores () {
+    return (
+    <div>
+        <h1>Fornecedores</h1>
+    </div>    
+    )
+}
+
---
+++ src/pages/lugarinho.jsx
@@ -0,0 +1,8 @@
+export default function lugarinho() {
+    return(
+    <div>
+        <h1>Lugarinho</h1>
+        <p>Lugarinho é uma pastelaria localizada em Guimarães</p>
+    </div>
+    )
+}
